73d1bbf09c4364935b655083706e9b281a528246
Rodrigo-etsidi <rodrigomoyasanz@gmail.com>
2026-05-10 18:34:10 +0200

    versión final
    Added new button callback functions for search and menu navigation.

--- src/botones.h
+++ src/botones.h
@@ -3,17 +3,13 @@
 
 #include <gtk/gtk.h>
 
-/* botones del menu principal */
 void on_btn_salir_clicked              (GtkButton *btn, gpointer datos);
 void on_btn_ir_listados_clicked        (GtkButton *btn, gpointer datos);
 void on_btn_ir_estadisticas_clicked    (GtkButton *btn, gpointer datos);
 void on_btn_ir_recomendaciones_clicked (GtkButton *btn, gpointer datos);
 void on_btn_ir_graficas_clicked        (GtkButton *btn, gpointer datos);
-
-/* panel listados */
-gboolean filtro_actividades_func (GtkTreeModel *modelo, GtkTreeIter *fila, gpointer datos);
-void on_btn_buscar_clicked        (GtkButton *btn,        gpointer datos);
-void on_btn_volver_menu_clicked   (GtkButton *btn,        gpointer datos);
-void on_txt_filtro_changed        (GtkSearchEntry *entry, gpointer datos);
+void on_btn_buscar_clicked             (GtkButton *btn, gpointer datos);
+void on_btn_volver_menu_clicked        (GtkButton *btn, gpointer datos);
+void on_txt_filtro_changed             (GtkSearchEntry *entry, gpointer datos);
 
 #endif
