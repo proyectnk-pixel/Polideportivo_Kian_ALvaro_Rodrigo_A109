11b0d82d0688c32e60d4def5001232b182efb7c8
Rodrigo-etsidi <rodrigomoyasanz@gmail.com>
2026-05-10 18:45:41 +0200

    versión final

---
+++ src/graficas.h
@@ -0,0 +1,17 @@
+#ifndef GRAFICAS_H
+#define GRAFICAS_H
+
+#include <gtk/gtk.h>
+#include <cairo.h>
+#include "ventana.h"
+
+/* callback que llama GTK cuando hay que redibujar el area */
+gboolean on_area_grafica_draw(GtkWidget *widget, cairo_t *cr, gpointer datos);
+
+/* barras de ocupacion media por centro */
+void dibujar_barras_centros(cairo_t *cr, int ancho, int alto, DatosVentana *v);
+
+/* barras de ocupacion media por dia de la semana */
+void dibujar_barras_dias(cairo_t *cr, int ancho, int alto, DatosVentana *v);
+
+#endif
