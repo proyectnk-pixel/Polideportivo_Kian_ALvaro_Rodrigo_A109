4eb9fe7d502e86cedafa6af19bf1cc45c9721058
Rodrigo-etsidi <rodrigomoyasanz@gmail.com>
2026-05-10 19:13:40 +0200

    Update graficas.h
    Updated comments in graficas.h for clarity.

--- src/graficas.h
+++ src/graficas.h
@@ -5,13 +5,13 @@
 #include <cairo.h>
 #include "ventana.h"
 
-/* callback que llama GTK cuando hay que redibujar el area */
+// callback, llama GTK cuando hay que redibujar el area 
 gboolean on_area_grafica_draw(GtkWidget *widget, cairo_t *cr, gpointer datos);
 
-/* barras de ocupacion media por centro */
+// barras ocupacion media por centro 
 void dibujar_barras_centros(cairo_t *cr, int ancho, int alto, DatosVentana *v);
 
-/* barras de ocupacion media por dia de la semana */
+// barras de ocupacion media por dia de la semana 
 void dibujar_barras_dias(cairo_t *cr, int ancho, int alto, DatosVentana *v);
 
 #endif
