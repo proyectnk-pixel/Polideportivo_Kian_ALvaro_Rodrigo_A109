30c1825593cfe9defb4522a7e7c3dd58f6835acc
kian Yletyinen <proyect.nk@gmail.com>
2026-05-10 21:35:10 +0200

    mejorar comentarios

--- src/graficas.c
+++ src/graficas.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 
-gboolean on_area_grafica_draw(GtkWidget *widget, cairo_t *cr, gpointer datos) {
+// GTK llama a esta función cada vez que necesita redibujar el area de graficas
+void on_area_grafica_draw(GtkWidget *widget, cairo_t *cr, gpointer datos) {
     DatosVentana *v = (DatosVentana *)datos;
     int ancho;
     int alto;
@@ -15,10 +16,8 @@ gboolean on_area_grafica_draw(GtkWidget *widget, cairo_t *cr, gpointer datos) {
     alto  = gtk_widget_get_allocated_height(GTK_WIDGET(v->drawing_grafica));
 
     dibujar_barras_actividades(cr, ancho, alto, v);
-
-    return FALSE;
 }
-
+// Dibuja una barra horizontal por cada actividad del centro seleccionado
 void dibujar_barras_actividades(cairo_t *cr, int ancho, int alto, DatosVentana *v) {
     int i;
     char etiqueta[20];
@@ -31,7 +30,7 @@ void dibujar_barras_actividades(cairo_t *cr, int ancho, int alto, DatosVentana *
     int y;
     int w;
     char *centro;
-
+    // Fondo blanco(el predefinido)
     cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
     cairo_paint(cr);
 
@@ -39,7 +38,7 @@ void dibujar_barras_actividades(cairo_t *cr, int ancho, int alto, DatosVentana *
         return;
     }
 
-    // uso el combo para saber que centro mostrar
+     // Lee el centro que esta seleccioando en el combo(el mismo que en listados)
     centro = gtk_combo_box_text_get_active_text(v->combo_centros);
 
     if (centro == NULL) {
@@ -50,7 +49,7 @@ void dibujar_barras_actividades(cairo_t *cr, int ancho, int alto, DatosVentana *
         cairo_show_text(cr, "Selecciona un centro en Listados primero");
         return;
     }
-
+// Configuración de la grafica, margenes, tamaño de barra y espacio entre barras.
     margen_izq = 200;
     margen_top = 50;
     alto_barra = 24;
@@ -59,7 +58,7 @@ void dibujar_barras_actividades(cairo_t *cr, int ancho, int alto, DatosVentana *
 
     cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
 
-    // titulo con el nombre del centro
+    // Título con el nombre del centro
     cairo_set_font_size(cr, 13.0);
     cairo_set_source_rgb(cr, 0.1, 0.2, 0.5);
     cairo_move_to(cr, margen_izq, 25);
@@ -70,7 +69,7 @@ void dibujar_barras_actividades(cairo_t *cr, int ancho, int alto, DatosVentana *
     y = margen_top;
 
     for (i = 0; i < v->ds->total; i++) {
-        //  actividades del centro seleccionado
+        //  Únicamente las actividades del centro seleccionado
         if (strcmp(v->ds->actividades[i].centro, centro) != 0) {
             continue;
         }
@@ -83,7 +82,7 @@ void dibujar_barras_actividades(cairo_t *cr, int ancho, int alto, DatosVentana *
         cairo_move_to(cr, 5, y + alto_barra - 6);
         cairo_show_text(cr, etiqueta);
 
-        // barra horizontal 
+        // barra horizontal a la ocupacion de la actividad
         w = (int)(v->ds->actividades[i].ocupacion * ancho_max);
         cairo_set_source_rgb(cr, 0.2, 0.4, 0.8);
         cairo_rectangle(cr, margen_izq, y, w, alto_barra);
@@ -98,6 +97,6 @@ void dibujar_barras_actividades(cairo_t *cr, int ancho, int alto, DatosVentana *
         y += alto_barra + espacio;
     }
 
-    g_free(centro);
+    g_free(centro);    // libero el string del combo
     (void)alto;
 }
