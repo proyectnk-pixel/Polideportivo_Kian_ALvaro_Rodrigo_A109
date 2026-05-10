7b8242a976ebb976fb1296ba803d7ee0f555a9c6
Rodrigo-etsidi <rodrigomoyasanz@gmail.com>
2026-05-10 18:43:53 +0200

    versión final

---
+++ src/estadisticas.h
@@ -0,0 +1,10 @@
+#ifndef ESTADISTICAS_H
+#define ESTADISTICAS_H
+
+#include <gtk/gtk.h>
+#include "ventana.h"
+
+//rellena el ranking de centros por ocupacion media de mayor a menor 
+void rellenar_ranking_centros(DatosVentana *v);
+
+#endif
